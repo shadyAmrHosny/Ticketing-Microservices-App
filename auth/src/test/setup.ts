eb0e60cc325c14a8d7ff2b08197f2710e211fab9
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-07-11 23:30:03 +0300

    add helper function to send authenticated  requests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 import { app } from '../app';
 import request from "supertest"; // allow us to fake requests to the express app
 
+declare global {
+  var signin: () => Promise<string[]>;
+}
+
 let mongo: any;
 beforeAll(async () => { // this function will run before all tests
   process.env.JWT_KEY = 'asdfasdf';
@@ -27,3 +31,21 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+// @ts-ignore
+global.signin = async ()=>{
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const res= await request(app)
+      .post('/api/users/signup').
+      send({
+        email,
+        password
+      })
+      .expect(201)
+  const cookie = res.get('Set-Cookie');
+  return cookie;
+
+}
+
