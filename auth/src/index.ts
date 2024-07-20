1fe58dcde544a4e24548296f327f5c95aae254ca
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-07-20 17:26:08 +0300

    set the mongo url as env

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,8 +6,11 @@ const start=async ()=>{
     if (!process.env.JWT_KEY){
         throw new Error('the secret key not found in env')
     }
+    if (!process.env.MONGO_URL){
+        throw new Error('the MONGO_URL must be defined')
+    }
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('db connected');
     }catch (err){
         console.log(err);
