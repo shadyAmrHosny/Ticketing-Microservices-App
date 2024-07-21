47ec94912cc072010ea420aefda25641126659bb
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-07-22 01:01:28 +0300

    faking authentication during tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,15 +1,17 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
 import request from "supertest"; // allow us to fake requests to the express app
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
-beforeAll(async () => { // this function will run before all tests
+beforeAll(async () => {  // this function will run before all tests
   process.env.JWT_KEY = 'asdfasdf';
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
   const mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
@@ -32,20 +34,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-// @ts-ignore
-global.signin = async ()=>{
-  const email = 'test@test.com'
-  const password = 'password'
+global.signin = () => {
+  // Build a JWT payload.  { id, email }
+  const payload = {
+    id: '1lk24j124l',
+    email: 'test@test.com'
+  };
 
-  const res= await request(app)
-      .post('/api/users/signup').
-      send({
-        email,
-        password
-      })
-      .expect(201)
-  const cookie = res.get('Set-Cookie');
-  return cookie;
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-}
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
 
+  // Turn that session into JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
+};
