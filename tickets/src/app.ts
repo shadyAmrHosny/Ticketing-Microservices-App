0a85171c0d5f4c7426e0a4499f930d123cd7dc54
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-07-23 03:25:11 +0300

    adding the routes to our express app

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,7 +5,9 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser} from '@shedzo_common/common';
 
 import {createTicketRouter} from "./routes/new";
-
+import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes";
+import {updateTicketRouter} from "./routes/update";
 
 
 const app = express();
@@ -20,6 +22,9 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 
 app.all('*', async (req, res) => {
