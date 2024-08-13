788f1e31b65131fbed4f40e9a0ed44658da52910
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-08-13 04:50:12 +0300

    testing order service

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,58 @@
+import request from 'supertest';
+import {app} from '../../app'
+import {Ticket} from "../../models/ticket";
+import {Order} from "../../models/order";
+
+const buildTicket = async () => {
+    const ticket  = Ticket.build({
+        title: "test",
+        price: 20
+    });
+    return await ticket.save();
+}
+
+it('Fetches orders for an particular user' , async ()=>{
+    const ticketOne = await buildTicket();
+    const ticketTwo = await buildTicket();
+    const ticketThree = await buildTicket();
+    const userOne  = global.signin();
+    const userTwo = global.signin();
+
+    //create order for user one
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', userOne)
+        .send({
+            ticketId: ticketOne.id
+        })
+        .expect(201);
+
+    //create two order for user two
+    const {body: orderOne} = await request(app)
+        .post('/api/orders')
+        .set('Cookie', userTwo)
+        .send({
+            ticketId: ticketThree.id
+        })
+        .expect(201);
+    const {body: orderTwo} = await request(app)
+        .post('/api/orders')
+        .set('Cookie', userTwo)
+        .send({
+            ticketId: ticketTwo.id
+        })
+        .expect(201);
+
+    //get second user orders
+
+    const res = await request(app)
+        .get('/api/orders')
+        .set('Cookie', userTwo)
+        .expect(200);
+
+    expect(res.body.length).toEqual(2);
+    expect(res.body[0].id).toEqual(orderOne.id);
+    expect(res.body[1].id).toEqual(orderTwo.id);
+
+
+})
---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest';
+import {app} from '../../app';
+import {Ticket} from '../../models/ticket';
+import mongoose from 'mongoose';
+import {OrderStatus} from '@shedzo_common/common';
+it('fetches order for a user ', async () => {
+    const ticket = Ticket.build({
+        title: 'test',
+        price: 100
+    });
+    await ticket.save();
+    const user = global.signin();
+    const {body: order} = await request(app)
+        .post('/api/orders')
+        .set('Cookie', user)
+        .send({
+            ticketId: ticket.id
+        })
+        .expect(201);
+
+    const {body: fetchOrder} = await request(app)
+        .get(`/api/orders/${order.id}`)
+        .set('Cookie', user)
+        .send()
+        .expect(200);
+
+    expect(order.id).toEqual(fetchOrder.id);
+});
