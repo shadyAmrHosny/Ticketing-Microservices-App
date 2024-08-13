b22a3dd017c86794d1b32b66233b13d017ea9d5c
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-08-13 04:38:28 +0300

    Publish TicketUpdated event

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from "express";
 import {body} from "express-validator";
 import {validateRequest, NotFoundError, requireAuth, NotAuthorizedError} from "@shedzo_common/common";
 import {Ticket} from "../models/ticket";
+import {TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -28,6 +30,13 @@ router.put('/api/tickets/:id',
     });
 
     await ticket.save();
+
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId
+    })
 
     res.send(ticket);
 });
