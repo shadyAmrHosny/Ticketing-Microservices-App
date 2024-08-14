433ef557dc936202d3945d0b9f3fb5995789a985
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-08-14 06:17:59 +0300

    start listening for events

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,12 +1,14 @@
 import mongoose from 'mongoose';
 import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
+import {TicketCreatedListener} from "./events/listeners/ticket-created-listener";
+import {TicketUpdatedListener} from "./events/listeners/ticket-updated-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
-  if (!process.env.MONGO_URI) {
+  if (!process.env.MONGO_URL) {
     throw new Error('MONGO_URI must be defined');
   }
   if (!process.env.NATS_CLIENT_ID) {
@@ -32,7 +34,10 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
-    await mongoose.connect(process.env.MONGO_URI);
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new   TicketUpdatedListener(natsWrapper.client).listen();
+
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
