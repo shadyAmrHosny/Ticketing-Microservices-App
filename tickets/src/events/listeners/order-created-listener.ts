9204c2d133c66091a07cf6b8cb8afa74ec9fbd3d
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-09-11 20:45:54 +0300

    reserving a ticket

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,29 @@
+import {Listener, OrderCreatedEvent, Subjects} from "@shedzo_common/common";
+import {queueGroupName} from "./queue-group-name";
+import {Message} from "node-nats-streaming";
+import {Ticket} from "../../models/ticket";
+import {TicketUpdatedPublisher} from "../publishers/ticket-updated-publisher";
+export class OrderCreatedListener extends Listener<OrderCreatedEvent>{
+    readonly subject = Subjects.OrderCreated;
+    queueGroupName= queueGroupName;
+    async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+        // Find the ticket that order is reserving
+        const ticket= await Ticket.findById(data.ticket.id);
+
+        // If no ticket, throw error
+        if (!ticket){
+            throw new Error(('Ticket not found'))
+        }
+
+        // Mark the ticket as being reserved by setting its orderId property
+        ticket.set({ orderId : data.id});
+
+        // Save the ticket
+        await ticket.save();
+
+
+        // ack the message
+        msg.ack();
+    }
+
+}
