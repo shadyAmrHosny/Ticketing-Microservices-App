ee357971506d502c15107aa92c39d24d377b9b4e
shadyAmrHosny <shadyamrhosny@gmail.com>
2024-09-14 16:19:50 +0300

    start listening

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,4 +1,5 @@
 import {natsWrapper} from "./nats-wrapper";
+import {OrderCreatedListener} from "./events/listeners/order-created-listener";
 const start=async ()=>{
     if (!process.env.NATS_CLIENT_ID){
         throw  new Error('NATS_CLIENT_ID must be defined');
@@ -22,6 +23,8 @@ const start=async ()=>{
         })
         process.on('SIGINT', () => natsWrapper.client.close());
         process.on('SIGTERM', () => natsWrapper.client.close());
+
+        new OrderCreatedListener(natsWrapper.client).listen();
 
     }catch (err){
         console.log(err);
